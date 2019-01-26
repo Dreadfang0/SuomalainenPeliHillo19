fac142624ca9aa9769990a8f864f01351413251c
Dreadfang0 <43678024+Dreadfang0@users.noreply.github.com>
2019-01-26 10:27:12 +0200

    File stuff

---
+++ SuomalainenPelihillo2019/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+    public GameHandler GM;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
