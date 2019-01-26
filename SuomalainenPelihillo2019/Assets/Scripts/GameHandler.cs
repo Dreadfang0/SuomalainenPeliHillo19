aa4bd92f1b457a5f4b49c0e00f1fedf382bbab2d
Dreadfang0 <43678024+Dreadfang0@users.noreply.github.com>
2019-01-26 11:32:13 +0200

    Interaction system framework

--- SuomalainenPelihillo2019/Assets/Scripts/GameHandler.cs
+++ SuomalainenPelihillo2019/Assets/Scripts/GameHandler.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour {
 
+    public int Weekday;
+    int GameState;
     public int Money;
+    int PersonLikeYou;
+    
+    [SerializeField]
+    GameObject[] HomeFurniture;
 	// Use this for initialization
 	void Start ()
     {
@@ -16,4 +22,52 @@ public class GameHandler : MonoBehaviour {
     {
 		
 	}
+    public void FurnitureActivator(int Furniture)
+    {
+        if (Furniture == 0)
+        {
+            HomeFurniture[0].SetActive(true);
+        }
+        if (Furniture == 1)
+        {
+            HomeFurniture[1].SetActive(true);
+        }
+        if (Furniture == 2)
+        {
+            HomeFurniture[2].SetActive(true);
+        }
+        if (Furniture == 3)
+        {
+            HomeFurniture[3].SetActive(true);
+        }
+        if (Furniture == 4)
+        {
+            HomeFurniture[4].SetActive(true);
+        }
+
+    }
+    public void PersonInteraction(int Person, bool Busy)
+    {
+        if (Person == 0)
+        {
+            if (Busy == false)
+            {
+                PersonLikeYou++;
+                Debug.Log("eyyyyy Lmao");
+            }
+            else
+            {
+                Debug.Log("Im bUsY");
+            }
+        }
+    }
+    public void DayChange()
+    {
+        Weekday++;
+        if (Weekday == 7)
+        {
+            GameState++;
+            Weekday = 0;
+        }
+    }
 }
--- SuomalainenPelihillo2019/Assets/Scripts/InteractableObject.cs
+++ SuomalainenPelihillo2019/Assets/Scripts/InteractableObject.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractableObject : MonoBehaviour {
+
+    [SerializeField]
+    bool IsFurniture;
+    [SerializeField]
+    int FurnitureID;
+    [SerializeField]
+    int FurnitureValue;
+    [SerializeField]
+    int PersonID;
+    [SerializeField]
+    int PersonInteractionPerDay;
+
     public GameHandler GM;
-	// Use this for initialization
-	void Start () {
+    
+    void Start ()
+    {
 		
 	}
 	
@@ -15,9 +28,41 @@ public class InteractableObject : MonoBehaviour {
 	}
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (collision.tag == "Player")
         {
-
+            
+            if (IsFurniture == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    if (GM.Money >= FurnitureValue)
+                    {
+                        GM.FurnitureActivator(FurnitureID);
+                        Debug.Log("your furniture has done a thing");
+                        GM.Money = GM.Money - FurnitureValue;
+                        gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        Debug.Log("You poor, Boi");
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    if (PersonInteractionPerDay > 0)
+                    {
+                        GM.PersonInteraction(PersonID, false);
+                        PersonInteractionPerDay--;
+                    }
+                    else
+                    {
+                        GM.PersonInteraction(PersonID, true);
+                    }
+                }
+            }
         }
     }
 }
