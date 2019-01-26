229131e9b3ce7941ffc01113366b1c2497c62dd9
Samu <samu.haaja@hotmail.com>
2019-01-26 11:50:39 +0200

    Alustavia scriptejä

---
+++ SuomalainenPelihillo2019/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 targetOffset;
+    [SerializeField] // [SerializeField] mahdollistaa arvojen vaihtamisen unityssä Privatesta huolimatta
+    private float movementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+    void MoveCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + targetOffset, movementSpeed * Time.deltaTime); 
+        // Camera follows the player based on position+offset with a set movement speed
+    }
+}
---
+++ SuomalainenPelihillo2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public static PlayerController instance;
+    [SerializeField] // Komento = Alla olevaa arvoa voi muuttaa Unityssä
+    private float movementSpeed;
+	void Start () {
+        instance = this;
+	}
+	
+	
+	void Update () {
+        HandleMovementInput();
+        //HandleRotationInput();
+       
+    }
+    void HandleMovementInput()
+    {
+        float _horizontal = Input.GetAxis("Horizontal");  //Get input for horizontal and vertical from Unity
+        float _vertical = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(_horizontal, _vertical, 0);
+        transform.Translate(_movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+    /*void HandleRotationInput()
+    {
+        RaycastHit _hit;
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Follow mouse
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
+        }
+    }*/
+    
+}
