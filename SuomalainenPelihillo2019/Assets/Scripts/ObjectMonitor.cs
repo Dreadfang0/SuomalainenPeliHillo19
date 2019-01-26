3b12ca8c6a5a79c22f95ed1e8da6551991c4f851
Samu <samu.haaja@hotmail.com>
2019-01-26 23:45:18 +0200

    The walls are caving in?!?

---
+++ SuomalainenPelihillo2019/Assets/Scripts/ObjectMonitor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectMonitor : MonoBehaviour {
+
+    public Collider2D playerAbove, playerBelow;
+    public static Transform player;
+    SpriteRenderer SR;
+
+    public void Awake()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        SR = this.GetComponent<SpriteRenderer>();
+    }
+
+    
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(this.transform.position, player.position) < 2000.0f)
+        {
+            decideColliderToUse();
+            decideSortingOrder();
+        }
+	}
+    void decideSortingOrder()
+    {
+        SR.sortingOrder = (int)Camera.main.WorldToScreenPoint(this.transform.position).y * -1;
+    }
+
+    void decideColliderToUse()
+    {
+        if (player.transform.position.y > this.transform.position.y)
+        {
+            playerAbove.enabled = true;
+            playerBelow.enabled = false;
+        }
+        else
+        {
+            playerAbove.enabled = false;
+            playerBelow.enabled = true;
+        }
+    
+    }
+
+}
